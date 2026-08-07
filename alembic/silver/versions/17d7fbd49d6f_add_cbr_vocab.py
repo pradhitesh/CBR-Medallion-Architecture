24fde0593493cd184de10c4161c8313486fedd2c
pradhitesh <pradhanhitesh@cbr-iisc.ac.in>
2026-08-07 13:45:47 +0530

    [feat] added cbr-vocab

---
+++ alembic/silver/versions/17d7fbd49d6f_add_cbr_vocab.py
@@ -0,0 +1,78 @@
+"""add cbr vocab
+
+Revision ID: 17d7fbd49d6f
+Revises: f2d32a214ea2
+Create Date: 2026-08-07 13:27:26.976715
+
+"""
+import datetime
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '17d7fbd49d6f'
+down_revision: Union[str, Sequence[str], None] = 'f2d32a214ea2'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+SCHEMA = "shared"
+# See issue: https://github.com/pradhitesh/CBR-OMOP-Development/issues/29
+VOCAB_CONCEPT_ID = 2000099999
+
+
+def upgrade() -> None:
+    conn = op.get_bind()
+
+    conn.execute(sa.text(f"""
+        INSERT INTO {SCHEMA}.concept (
+            concept_id,
+            concept_name,
+            domain_id,
+            vocabulary_id,
+            concept_class_id,
+            standard_concept,
+            concept_code,
+            valid_start_date,
+            valid_end_date,
+            invalid_reason
+        ) VALUES (
+            :concept_id,
+            'CBR-SANSCOG Vocabulary',
+            'Metadata',
+            'Vocabulary',
+            'Vocabulary',
+            NULL,
+            'CBR',
+            :today,
+            '2099-12-31',
+            NULL
+        )
+    """), {"concept_id": VOCAB_CONCEPT_ID, "today": datetime.date.today()})
+
+    conn.execute(sa.text(f"""
+        INSERT INTO {SCHEMA}.vocabulary (
+            vocabulary_id,
+            vocabulary_name,
+            vocabulary_reference,
+            vocabulary_version,
+            vocabulary_concept_id
+        ) VALUES (
+            'CBR',
+            'CBR-SANSCOG Custom Vocabulary',
+            'https://cbr-iisc.ac.in/',
+            'v1.0',
+            :vocab_concept_id
+        )
+    """), {"vocab_concept_id": VOCAB_CONCEPT_ID})
+
+
+def downgrade() -> None:
+    conn = op.get_bind()
+    conn.execute(sa.text(f"DELETE FROM {SCHEMA}.vocabulary WHERE vocabulary_id = 'CBR'"))
+    conn.execute(sa.text(f"DELETE FROM {SCHEMA}.concept WHERE concept_id = :concept_id"),
+                 {"concept_id": VOCAB_CONCEPT_ID})
+
